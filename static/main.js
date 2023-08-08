78d9ee1e531433023b0b80d44501cb0791435e1c
Wumbo <58399748+WumboSpasm@users.noreply.github.com>
2023-08-07 21:26:41 -0400

    Refactor/reorganize code
    Zipped VRML/X3D entries currently don't work due to X_ITE's lack of support for object URLs.

--- static/main.js
+++ static/main.js
@@ -1,9 +1,31 @@
+let entry = null;
+let gameZip = null;
+
 const oooo = 'https://api.ooooooooo.ooo';
 const fpdb = 'https://db-api.unstable.life';
 
-// Create unmodified copies of methods that will be redirected
-const _fetch = window.fetch;
-const _createElement = document.createElement;
+// Automatically convert URL objects to their redirected equivalents
+const redirect = async request => {
+    let url = {
+        // The requested URL, adjusted to use the launch command as the base rather than the current domain
+        original: new URL(request.origin == location.origin ? request.pathname.substring(1) : request.href, entry.launchCommand),
+        // The actual URL from which the requested file will be retrieved
+        redirect: ''
+    };
+    
+    // If the entry is zipped and requested file exists inside zip, return object URL of file 
+    if (gameZip != null) {
+        let redirectedFile = gameZip.file(decodeURIComponent('content/' + url.original.hostname + url.original.pathname));
+        if (redirectedFile != null) {
+            url.redirect = URL.createObjectURL(await redirectedFile.async('blob'));
+            return url;
+        }
+    }
+    
+    // If entry is not zipped or requested file does not exist inside zip, return API request to be fetched later
+    url.redirect = `${fpdb}/get?url=${url.original.hostname + url.original.pathname}`;
+    return url;
+};
 
 // Player attributes and initialization methods
 const players = [
@@ -14,14 +36,40 @@ const players = [
         
         get override() { return window.RufflePlayer && window.RufflePlayer.sources.extension != null; },
         
-        initialize(launchCommand) {
+        async initialize() {
+            // Create copy of unmodified fetch method
+            const _fetch = window.fetch;
+            // Intercept calls to fetch with code that replaces the requested URL with redirected equivalent
+            window.fetch = async (resource, options) => {
+                // Get request as URL object
+                let resourceURL = new URL(resource instanceof Request ? resource.url : resource);
+                
+                // ??? (I think this was for some obscure edge case I can't find examples of anymore)
+                if (resourceURL.protocol == 'blob:')
+                    resourceURL = new URL(resourceURL.pathname);
+                
+                // Don't redirect if the requested URL belongs to the active player or doesn't use HTTP
+                if (this.source.startsWith(resourceURL.origin) || !resourceURL.protocol.startsWith('http'))
+                    return await _fetch(resource, options);
+                
+                // Get redirected URL and fetch
+                let redirectInfo = await redirect(resourceURL);
+                let response = await _fetch(redirectInfo.redirect, options);
+                
+                // Spoof URL to bypass sitelocks
+                Object.defineProperty(response, 'url', { value: redirectInfo.original.href });
+                
+                return response;
+            };
+            
+            // Create instance of player
             let player = window.RufflePlayer.newest().createPlayer();
             // Set base URL to path of launch command
-            player.config.base = launchCommand.substring(0, launchCommand.lastIndexOf('/'));
+            player.config.base = entry.launchCommand.substring(0, entry.launchCommand.lastIndexOf('/'));
             
             // Add player to DOM and load
             document.querySelector('.player').append(player);
-            player.load(launchCommand);
+            player.load(entry.launchCommand);
             
             // Once loaded, resize player to dimensions of SWF
             player.addEventListener('loadedmetadata', () => {
@@ -39,17 +87,37 @@ const players = [
         
         get override() { return false; },
         
-        initialize(launchCommand) {
+        async initialize() {
+            // Create copy of unmodified createElement method
+            const _createElement = document.createElement;
+            // Intercept calls to createElement with code that replaces the source value of <img> elements with redirected equivalent
+            document.createElement = function(...args) {
+                let observer = new MutationObserver(async records => {
+                    // Only redirect requests that haven't already been redirected yet
+                    let r = records.findIndex(record => !['blob:', fpdb].some(prefix => record.target.src.startsWith(prefix)));
+                    if (r != -1) records[r].target.src = (await redirect(new URL(records[r].target.src))).redirect;
+                });
+                
+                // Create the element
+                let element = _createElement.apply(this, args);
+                // If created element is an <img> element, observe changes to source value
+                if (element.tagName == 'IMG')
+                    observer.observe(element, { attributes: true, attributeFilter: ['src'] });
+                
+                return element;
+            };
+            
+            // Create instance of player
             let player = X3D.createBrowser();
             // There's no way to identify the intended dimensions of a VRML/X3D file, so always resize to 900x600
             player.style.width = '900px';
             player.style.height = '600px';
             // Set base URL to path of launch command
-            player.browser.baseURL = launchCommand.substring(0, launchCommand.lastIndexOf('/'));
+            player.browser.baseURL = entry.launchCommand.substring(0, entry.launchCommand.lastIndexOf('/'));
             
             // Add player to DOM and load
             document.querySelector('.player').append(player);
-            player.browser.loadURL(new X3D.MFString(launchCommand));
+            player.browser.loadURL(new X3D.MFString((await redirect(new URL(entry.launchCommand))).redirect));
         }
     }
 ];
@@ -66,7 +134,6 @@ else if (localStorage.getItem('filter') != 'false')
 // Fetch API request
 fetch(request).then(async response => {
     // Deserialize JSON response
-    let entry;
     try {
         entry = await response.json();
     // If deserialization fails, replace page content with error message
@@ -109,93 +176,32 @@ fetch(request).then(async response => {
     
     // Don't load a second instance of player if it's already active (ie. by using an extension)
     if (players[p].override)
-        playEntry(entry, p);
-    // Otherwise, load player by appending <script> element to page
+        prepareEntry();
     else {
+        // Load player by appending <script> element to page
         let script = document.createElement('script');
         script.src = players[p].source;
-        script.addEventListener('load', () => playEntry(entry, p));
+        script.addEventListener('load', prepareEntry);
         document.head.append(script);
     }
-});
-
-async function playEntry(entry, p) {
-    let gameZip = null;
-    // If the entry is zipped, retrieve zip from API and load into JSZip
-    try {
-        if (entry.zipped) gameZip = await new JSZip().loadAsync(await fetch(`${fpdb}/get?id=${entry.uuid}`).then(r => r.blob()));
-    // If there are issues retrieving/loading the zip, replace content with error message
-    } catch {
-        let player = document.querySelector('.player');
-        player.style.fontSize = '12px';
-        player.style.padding = '16px 0 20px';
-        player.textContent = 'Failed to load entry. This is not an emulator issue.';
-        return;
-    }
     
-    // Automatically convert URL objects to their redirected equivalents
-    let redirect = async request => {
-        let url = {
-            // The requested URL, adjusted to use the launch command as the base rather than the current domain
-            original: new URL(request.origin == location.origin ? request.pathname.substring(1) : request.href, entry.launchCommand),
-            // The actual URL from which the requested file will be retrieved
-            redirect: ''
-        };
-        
-        // If the entry is zipped and requested file exists inside zip, return object URL of file 
+    // Get zip if needed, and begin setting up player and redirector
+    async function prepareEntry() {
         if (entry.zipped) {
-            let redirectedFile = gameZip.file(decodeURIComponent('content/' + url.original.hostname + url.original.pathname));
-            if (redirectedFile != null) {
-                url.redirect = URL.createObjectURL(await redirectedFile.async('blob'));
-                return url;
+            // If the entry is zipped, retrieve zip from API and load into JSZip
+            try {
+                gameZip = await new JSZip().loadAsync(await fetch(`${fpdb}/get?id=${entry.uuid}`).then(r => r.blob()));
+            // If there are issues retrieving/loading the zip, display error message in place of player
+            } catch {
+                let player = document.querySelector('.player');
+                player.style.fontSize = '12px';
+                player.style.padding = '16px 0 20px';
+                player.textContent = 'Failed to load entry. This is not an emulator issue.';
+                return;
             }
         }
         
-        // If entry is not zipped or requested file does not exist inside zip, return API request to be fetched later
-        url.redirect = `${fpdb}/get?url=${url.original.hostname + url.original.pathname}`;
-        return url;
-    };
-    
-    // Intercept calls to Fetch API with code that replaces the requested URL with redirected equivalent (required for Ruffle)
-    window.fetch = async (resource, options) => {
-        // Get request as URL object
-        let resourceURL = new URL(resource instanceof Request ? resource.url : resource);
-        
-        // ??? (I think this was for some obscure edge case I can't find examples of anymore)
-        if (resourceURL.protocol == 'blob:')
-            resourceURL = new URL(resourceURL.pathname);
-        
-        // Don't redirect if the requested URL belongs to the active player or doesn't use HTTP
-        if (players[p].source.startsWith(resourceURL.origin) || !resourceURL.protocol.startsWith('http'))
-            return await _fetch(resource, options);
-        
-        // Get redirected URL and forward to unmodified Fetch API
-        let redirectInfo = await redirect(resourceURL),
-            response = await _fetch(redirectInfo.redirect, options);
-        
-        // Spoof URL to bypass sitelocks
-        Object.defineProperty(response, 'url', { value: redirectInfo.original.href });
-        
-        return response;
-    };
-    
-    // Intercept calls to createElement method with code that replaces the source value of <img> elements with redirected equivalent (required for X_ITE)
-    document.createElement = function(...args) {
-        let observer = new MutationObserver(async records => {
-            // Only redirect requests that haven't already been redirected yet
-            let r = records.findIndex(record => !['blob:', fpdb].some(prefix => record.target.src.startsWith(prefix)));
-            if (r != -1) records[r].target.src = (await redirect(new URL(records[r].target.src))).redirect;
-        });
-        
-        // Create the element 
-        let element = _createElement.apply(this, args);
-        // If created element is an <img> element, observe changes to source value
-        if (element.tagName == 'IMG')
-            observer.observe(element, { attributes: true, attributeFilter: ['src'] });
-        
-        return element;
-    };
-    
-    // Load player now that requests are being actively redirected
-    players[p].initialize(entry.launchCommand);
-}
+        // Add player to DOM and activate redirector
+        players[p].initialize();
+    }
+});

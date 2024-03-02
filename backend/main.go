d3c9b8d5ff1a10d6e567d1233149e54d8c48290a
Dri0m <dri0m@seznam.cz>
2024-03-02 15:38:40 +0100

    fix gamezip games

--- backend/main.go
+++ backend/main.go
@@ -119,7 +119,7 @@ func main() {
 	http.HandleFunc("/", rootHandler)
 	http.HandleFunc("/faq", faqHandler)
 	fs := http.FileServer(http.Dir("../static"))
-	http.Handle("/static/", http.StripPrefix("/static", fs))
+	http.Handle("/static/", NoCache(http.StripPrefix("/static", fs)))
 	http.HandleFunc("/static/browse", oldBrowseRedirectHandler)
 	http.HandleFunc("/static/browse/", oldBrowseRedirectHandler)
 
@@ -337,4 +337,42 @@ func writeMessage(w http.ResponseWriter, message string) {
 		log.Error().Err(err).Msg("failed write response to the user")
 		return
 	}
+}
+
+var epoch = time.Unix(0, 0).Format(time.RFC1123)
+
+var noCacheHeaders = map[string]string{
+	"Expires":         epoch,
+	"Cache-Control":   "no-cache, private, max-age=0",
+	"Pragma":          "no-cache",
+	"X-Accel-Expires": "0",
+}
+
+var etagHeaders = []string{
+	"ETag",
+	"If-Modified-Since",
+	"If-Match",
+	"If-None-Match",
+	"If-Range",
+	"If-Unmodified-Since",
+}
+
+func NoCache(h http.Handler) http.Handler {
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		// Delete any ETag headers that may have been set
+		for _, v := range etagHeaders {
+			if r.Header.Get(v) != "" {
+				r.Header.Del(v)
+			}
+		}
+
+		// Set our NoCache headers
+		for k, v := range noCacheHeaders {
+			w.Header().Set(k, v)
+		}
+
+		h.ServeHTTP(w, r)
+	}
+
+	return http.HandlerFunc(fn)
 }
